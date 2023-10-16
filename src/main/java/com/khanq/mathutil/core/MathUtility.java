5b85d8f4ed3b0b6a7f8ae20c52b7c7198380500a
yyenthaonek <yyenthaonek1507@gmail.com>
2023-10-16 08:35:15 +0700

    update getf() with 10 to see redalert

--- src/main/java/com/khanq/mathutil/core/MathUtility.java
+++ src/main/java/com/khanq/mathutil/core/MathUtility.java
@@ -29,7 +29,7 @@ public class MathUtility {
         if (n == 0 || n == 1) 
             return 1; // nếu n đặc biệt, ta return sớm luôn
         
-        long result = 1;//sống sót đến lệnh này, sure n = 0..20
+        long result = 10;//sống sót đến lệnh này, sure n = 0..20
 
         for (int i = 2; i <= n; i++)
             result *= i;
